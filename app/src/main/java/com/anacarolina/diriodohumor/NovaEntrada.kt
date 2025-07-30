82c4399fec8ff8f0b545b40f1ef97026211dad66
Ana Carolina <a.caroly16@hotmail.com>
2025-07-30 01:43:19 -0300

    CardView para os registros - feito

--- app/src/main/java/com/anacarolina/diriodohumor/NovaEntrada.kt
+++ app/src/main/java/com/anacarolina/diriodohumor/NovaEntrada.kt
@@ -1,4 +1,26 @@
 package com.anacarolina.diriodohumor
 
-class NovaEntrada {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Spinner
+
+class NovaEntrada : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nova_entrada)
+
+        val data = findViewById<EditText>(R.id.Edit_data)
+        val emocao1 = findViewById<RadioGroup>(R.id.radioGroup_emocao1)
+        val emocao2 = findViewById<RadioGroup>(R.id.radioGroup_emocao2)
+        val categoria = findViewById<Spinner>(R.id.Spinner_categoria)
+        val buttonSalvar = findViewById<Button>(R.id.button_salvar)
+
+        buttonSalvar.setOnClickListener {
+
+        }
+
+    }
 }
